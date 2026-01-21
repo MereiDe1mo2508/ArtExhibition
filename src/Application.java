591087465623507cf2323951129b19b9289d5641
merei <254292@astanait.edu.kz>
2026-01-21 11:44:50 +0500

    com

--- src/Application.java
+++ src/Application.java
@@ -8,6 +8,7 @@ void main() throws Exception {
         IO.println("1. Get all artworks: ");
         IO.println("2. Get artwork by id: ");
         IO.println("3. Create artwork: ");
+        IO.println("4. Delete artwork: ");
         IO.println("0. Exit");
         try {
             IO.print("Enter your choice (1-3): ");
@@ -28,12 +29,27 @@ void main() throws Exception {
                 preparedStatement.setInt(1, id);
                 preparedStatement.executeUpdate();
             } else if (choice == 3) {
-                String sql = "INSERT INTO artwork(title, artist, date_of_creating, copyrighted) VALUES (?, ?, ?, ?)";
+                String sql = "INSERT INTO artwork(artwork_id, title, artist, date_of_creating, copyrighted) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                scanner.nextLine();
+                IO.println("Input integer: ");
+                preparedStatement.setInt(1, Integer.parseInt(scanner.nextLine()));
+                IO.println("Input title: ");
+                preparedStatement.setString(2, scanner.nextLine());
+                IO.println("Input artist: ");
+                preparedStatement.setString(3, scanner.nextLine());
+                IO.println("Input date_of_creating: ");
+                preparedStatement.setInt(4, Integer.parseInt(scanner.nextLine()));
+                IO.println("Input copyrighted: ");
+                preparedStatement.setBoolean(5, scanner.nextBoolean());
+                preparedStatement.executeUpdate();
+            } else if (choice == 4) {
+                String sql = "DELETE FROM artwork WHERE artwork_id = ?;";
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
                 IO.println("Input integer: ");
                 scanner.nextLine();
-                String title = scanner.nextLine();
-                preparedStatement.setString(1, title);
+                int id = Integer.parseInt(scanner.nextLine());
+                preparedStatement.setInt(1, id);
                 preparedStatement.executeUpdate();
             } else if (choice == 0) {
                 System.exit(0);
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
-import controllers.artworkController;
-import data.PostgresDB;
-import data.interfaces.IDB;
-import repositories.artworkRepository;
-import repositories.interfaces.IartworkRepository;
-
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.InputMismatchException;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
